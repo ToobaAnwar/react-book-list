338098eee77e1e2fbfe34c33928fce05509402a5
Tooba Anwar <muznahaq@gmail.com>
2020-03-02 19:54:20 +0500

    first commit

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useReducer, useEffect } from 'react';
+import uuid from 'uuid/v1'
+import { bookReducer } from '../reducers/bookReducer';
+// npm install uuid
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : []
+    });
+     useEffect(()=> {
+         localStorage.setItem('books', JSON.stringify(books))
+     }, [books])
+    return(
+        <BookContext.Provider value={{ books, dispatch }}>
+         { props.children }
+        </BookContext.Provider>
+    )
+}
+ 
+export default BookContextProvider;
